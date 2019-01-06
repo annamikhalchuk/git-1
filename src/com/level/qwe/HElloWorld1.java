04d5793e1b554ff1c6dfd4f65f9fbc32190ea337
annamikhalchuk <ai.mikhalchuk@gmail.com>
2019-01-07 01:15:45 +0200

    Два файла добавлено

---
+++ src/com/level/qwe/HElloWorld1.java
@@ -0,0 +1,7 @@
+package com.level.qwe;
+
+public class HElloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello? World!");
+    }
+}
---
+++ src/com/level/qwe/HelloWorld2.java
@@ -0,0 +1,7 @@
+package com.level.qwe;
+
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World2!!!!!");
+    }
+}
