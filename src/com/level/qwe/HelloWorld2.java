e372dd852a4d8c93fe288521e5d501dd814f87b0
annamikhalchuk <46426378+annamikhalchuk@users.noreply.github.com>
2019-01-08 01:33:34 +0200

    Update HelloWorld2.java

--- src/com/level/qwe/HelloWorld2.java
+++ src/com/level/qwe/HelloWorld2.java
@@ -2,6 +2,6 @@ package com.level.qwe;
 
 public class HelloWorld2 {
     public static void main(String[] args) {
-        System.out.println("Hello, World2!!!!!");
+        System.out.println("Hello, World 222 !!!!!");
     }
 }
